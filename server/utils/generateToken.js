e4aea74bf90f38566d26c1f686e2078d68cf2b17
mihiragath <mihiragath@users.noreply.github.com>
2026-04-16 15:14:11 +0530

    add

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,8 +9,13 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      secure: true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000,
     })
-    .json({ success: true, message, user });
+    .json({
+      success: true,
+      message,
+      user,
+    });
 };
