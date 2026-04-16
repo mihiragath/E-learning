ad3f807fef2b746cb5c325db6f655933c89bceab
mihiragath <mihiragath@users.noreply.github.com>
2026-04-16 13:25:39 +0530

    add backend api

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,7 +1,7 @@
 import CreateLecture from "@/pages/admin/lecture/CreateLecture";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "http://localhost:8080/api/v1/course";
+const COURSE_API = "https://e-learning-backend-p2yp.onrender.com/api/v1/course";
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
